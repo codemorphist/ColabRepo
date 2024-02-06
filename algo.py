d0ebb9afdb2fc6e1db44a50a69ca1349cb237a36
Alex Katrenko <alexkatrenkos@gmail.com>
2024-02-06 21:30:35 +0200

    Added Boyer–Moore majority vote algorithm to algo.py

--- algo.py
+++ algo.py
@@ -1,2 +1,20 @@
 def test():
     print("Test")
+
+def majority_element(nums: list[int]) -> int:
+    """
+    Boyer–Moore majority vote algorithm
+    """
+    c: int = 1
+    el: int = nums[0]
+
+    for i in range(1, len(nums)+1):
+        if el == nums[i]:
+            c += 1
+        else:
+            c -= 1
+            if not c:
+                el = nums[i]
+                c = 1
+    return el
+
