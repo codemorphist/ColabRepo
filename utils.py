fc7f538da7e2ffb000819bbe3386fb35c6114a4d
MargoAndrianova <157409841+MargoAndrianova@users.noreply.github.com>
2024-02-06 11:35:02 +0200

    Added def isPrime

--- utils.py
+++ utils.py
@@ -7,3 +7,9 @@ def fac(n: int) -> int:
         cahed[n] = res
         return res
 
+def isPrime(n: int):
+    for i in range(2, int(n**0.5)+1):
+        if n % i == 0:
+            return False
+    return True
+
