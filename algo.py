5a209d0fbee9171a3c6ee4a9d83ecc33b02d2768
MargoAndrianova <157409841+MargoAndrianova@users.noreply.github.com>
2024-02-06 21:39:15 +0200

    Added def double_letter to algo.py

--- algo.py
+++ algo.py
@@ -1,2 +1,11 @@
 def test():
     print("Test")
+
+def double(lst):
+    res = ""
+
+    for i in lst:
+        res += i
+        if i in "abcdefghijklmnopqrstuvwxyz":
+            res += i
+    return res 
